e8a970664e62bf9d4d1fb1dccbab5f6f0654fd58
Ivan Stepinac <ivanstepinac@Ivans-MacBook-Pro.local>
2025-10-12 14:17:03 +0200

    first task

--- vj1/zad1.c
+++ vj1/zad1.c
@@ -1,3 +1,12 @@
+/*
+ Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata
+ zapisanih u datoteci. Nakon toga potrebno je dinamički alocirati prostor za niz struktura
+ studenata (ime, prezime, bodovi) i učitati iz datoteke sve zapise. Na ekran ispisati ime,
+ prezime, apsolutni i relativni broj bodova.
+ Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+ relatvan_br_bodova = br_bodova/max_br_bodova*100
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #define ERROR_OPENING_FILE 1
