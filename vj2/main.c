6663a31abff5eb4ea49ecb46353f75f974fad34e
Ivan Stepinac <ivanstepinac@Ivans-MacBook-Pro.local>
2025-10-17 20:28:29 +0200

    vjezba 2

---
+++ vj2/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  vj2
+//
+//  Created by Ivan Stepinac on 17.10.2025..
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int a;
+    printf("Unesi broj!\n");
+
+    scanf("%d",&a);
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
